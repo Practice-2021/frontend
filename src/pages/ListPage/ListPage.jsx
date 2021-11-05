e1fc7e4f3788a98cadd625406d62a17cf61022bf
azhukov <azhukov@emphasoft.com>
2021-11-05 20:08:25 +0300

    checking ci/cd

--- src/pages/ListPage/ListPage.jsx
+++ src/pages/ListPage/ListPage.jsx
@@ -3,7 +3,7 @@ import Search from "../../components/Search";
 import TeacherList from "../../components/TeacherList";
 const ListPage = () => {
     return (
-        <Layout className="w-full md:h-full bg-white filter drop-shadow-xl pb-8">
+        <Layout className="w-full md:h-full bg-red-500 filter drop-shadow-xl pb-8">
             <Search className="flex justify-center pt-4"/>
             <TeacherList className="flex flex-wrap justify-between mt-12 mx-8 md:mx-12 lg:mx-24 xl:mx-40 2xl:mx-48 "/>
         </Layout>
