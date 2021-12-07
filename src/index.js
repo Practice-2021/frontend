a8e41ab85dc27db655f3192df507105d6f4e515e
Andrew Zhukov <45205783+NixoN2@users.noreply.github.com>
2021-12-07 20:11:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,15 +7,6 @@ import store from "./store";
 import {Provider} from "react-redux";
 import {ApolloProvider} from "@apollo/client";
 import {client} from "./apollo";
-import { ReactSearchKit,  ESSearchApi} from 'react-searchkit';
-
-const searchApi = new ESSearchApi({
-  axios: {
-    url: 'http://159.253.21.180:4000/employee',
-    timeout: 5000,
-  }
-});
-
 
 
 ReactDOM.render(
@@ -23,9 +14,7 @@ ReactDOM.render(
     <Router>
       <Provider store={store}>
         <ApolloProvider client={client}>
-          <ReactSearchKit searchApi={searchApi}>
             <App />
-          </ReactSearchKit>
         </ApolloProvider>
       </Provider>
     </Router>
